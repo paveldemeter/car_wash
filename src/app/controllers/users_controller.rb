a5ca1518ca362a240c181ed5eaae1828e1190c7e
Pavel Demeter <paveldemeter@hotmail.com>
2019-11-12 14:18:51 +1000

    fixed heroku issue

--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < Devise::RegistrationsController
     before_action :configure_permitted_parameters
+    before_action :redirect_from_index, only: [:index]
 
     def create
       super
@@ -7,10 +8,17 @@ class UsersController < Devise::RegistrationsController
       current_user.roles << Role.find_by(name: 'washer') if params[:user][:roles] == '1'
       current_user.save
     end
+
+    def index
+    end
   
     protected
   
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up).push(:image)  
+    end
+
+    def redirect_from_index
+      redirect_to root_path  
     end
   end
